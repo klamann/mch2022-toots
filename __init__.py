3fe655336badd9a7ba5f8a3f1eb2b25870f1d93b
Sebastian Straub <sstraub@posteo.de>
2022-07-23 16:18:10 +0200

    code cleanup and more button bindings for navigation

--- __init__.py
+++ __init__.py
@@ -1,16 +1,20 @@
+import gc
 import json
-import system, wifi, time, display, gc
-from time import sleep, localtime
-import urequests as requests
-import buttons
-import mch22
 import re
+import time
+
+import buttons
+import display
+import mch22
+import system
+import urequests as requests
+import wifi
 
 sample_json = ""
 
-DEBUG = True
-toot_counter = 0
-
+# mastadon api response and current selection
+toots = []
+toot_index = 0
 # a translation table for common characters that the badge has issues with (str.translate is not supported)
 char_replacements = {'ä': 'ae', 'ö': 'oe', 'ü': 'ue', 'ß': 'ss', 'Ä': 'Ae', 'Ö': 'Oe', 'Ü': 'Ue'}
 # some common html entities, since we don't have a full html unescape function in micropython
@@ -27,7 +31,7 @@ max_text_width = display.width() - 10
 line_margin = 3
 
 
-def connectToWifi():
+def wlan_connect():
     if not wifi.status():
         wifi.connect()
         display.drawFill(display.WHITE)
@@ -37,7 +41,7 @@ def connectToWifi():
             system.home()
 
 
-def drawSplash():
+def draw_splash():
     display.drawFill(display.WHITE)
     display.drawText(10, 10, "Getting #mch2022 toots from chaos.social ...", 0x000000, font)
     display.flush()
@@ -51,7 +55,6 @@ def fetch_toots():
     return data
 
 
-
 def button_exit_app(pressed):
     if pressed:
         mch22.exit_python()
@@ -59,34 +62,58 @@ def button_exit_app(pressed):
 
 def callback_next_toot(p):
     if p:
-        global toot_counter
-        if toot_counter >= len(data):
-            toot_counter = 0
-        print(f"toot #{toot_counter}")
-        print_toot(data[toot_counter])
-        toot_counter += 1
+        switch_toot(toot_index + 1)
 
 
-def isascii(c):
-    return 0 <= ord(c) <= 0x7F
+def callback_prev_toot(p):
+    if p:
+        switch_toot(toot_index - 1)
+
+
+def switch_toot(index):
+    if index >= len(toots):
+        index = 0
+    if index < 0:
+        index = len(toots) - 1
+    print(f"toot #{index}")
+    global toot_index
+    toot_index = index
+    print_toot(toots[index])
 
 
 def print_toot(toot_json):
+    # get content
     content_html = toot_json['content']
-    content_plain = re.sub('<br.*?>', ' ', content_html)
-    content_plain = re.sub('<[^<]+?>', '', content_plain)
-    content_plain = ''.join(char_replacements.get(c, c) for c in content_plain)
-    content_plain = ''.join(c if isascii(c) else '?' for c in content_plain)
-    if '&' in content_plain:
-        for html_entity, replacement in html_entities.items():
-            content_plain = content_plain.replace(html_entity, replacement)
+    content_plain = simplify_text(content_html)
     author = toot_json['account']['username']
     created = toot_json['created_at']
-
+    # author & date
     display.drawFill(0xFFFFFF)
-    display.drawText(5, 5, f"{toot_counter+1}/{len(data)}: @{author}, {created}:", 0x000000, font)
+    display.drawText(5, 5, f"{toot_index + 1}/{len(toots)}: @{author}, {created}:", 0x000000, font)
+    # print content lines
+    lines = text_to_lines(content_plain)
+    for i, line in enumerate(lines):
+        y = 10 + ((i + 1) * (font_height + line_margin))
+        display.drawText(5, y, line, 0x000000, font)
+    display.flush()
 
-    words = content_plain.split()
+
+def simplify_text(text):
+    # strip html tags
+    plain = re.sub('<br.*?>', ' ', text)
+    plain = re.sub('<[^<]+?>', '', plain)
+    # replace non-ascii chars (not supported by badge)
+    plain = ''.join(char_replacements.get(c, c) for c in plain)
+    plain = ''.join(c if isascii(c) else '?' for c in plain)
+    # replace common html entities
+    if '&' in plain:
+        for html_entity, replacement in html_entities.items():
+            plain = plain.replace(html_entity, replacement)
+    return plain
+
+
+def text_to_lines(text):
+    words = text.split()
     line = ''
     lines = []
     for word in words:
@@ -99,31 +126,37 @@ def print_toot(toot_json):
             line = line_tmp
     if (not lines) or (lines[-1] != line):
         lines.append(line)
-
-    for i, line in enumerate(lines):
-        y = 10 + ((i + 1) * (font_height + line_margin))
-        display.drawText(5, y, line, 0x000000, font)
-
-    display.flush()
+    return lines
 
 
-buttons.attach(buttons.BTN_HOME, button_exit_app)
-if DEBUG:
-    drawSplash()
-    data = json.loads(sample_json)
-else:
-    connectToWifi()
-    drawSplash()
-    data = fetch_toots()
+def isascii(c):
+    return 0 <= ord(c) <= 0x7F
 
-if not data:
-    display.drawFill(display.WHITE)
-    display.drawText(10, 10, "No toots received :'(", 0x000000, font)
-    display.flush()
-else:
-    toot = data[toot_counter]
-    print_toot(toot)
-    toot_counter += 1
-    buttons.attach(buttons.BTN_A, callback_next_toot)
-    while True:
-        time.sleep(1)
+
+def main(debug=False):
+    global toots
+    buttons.attach(buttons.BTN_HOME, button_exit_app)
+    if debug:
+        draw_splash()
+        toots = json.loads(sample_json)
+    else:
+        wlan_connect()
+        draw_splash()
+        toots = fetch_toots()
+
+    if not toots:
+        display.drawFill(display.WHITE)
+        display.drawText(10, 10, "No toots received :'(", 0x000000, font)
+        display.flush()
+    else:
+        print_toot(toots[0])
+        buttons.attach(buttons.BTN_A, callback_next_toot)
+        buttons.attach(buttons.BTN_UP, callback_prev_toot)
+        buttons.attach(buttons.BTN_DOWN, callback_next_toot)
+        buttons.attach(buttons.BTN_LEFT, callback_prev_toot)
+        buttons.attach(buttons.BTN_RIGHT, callback_next_toot)
+        while True:
+            time.sleep(1)
+
+
+main(debug=True)
